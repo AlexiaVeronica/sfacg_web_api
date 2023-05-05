c5479d5ccd15bf5ebc9c24d27b61c5ade6eca7ad
Alexia <Elaina-Alex@proton.me>
2023-05-05 16:08:13 +0800

    ~

--- main.py
+++ main.py
@@ -1,12 +1,79 @@
 import sfacg_web_api
 
-if __name__ == '__main__':
+from fastapi import FastAPI, HTTPException, Depends
+from starlette.responses import HTMLResponse
 
-    sf = sfacg_web_api.SFACGBook("629008")
-    book_info = sf.get_book_info()
-    print(book_info)
-    chapter_list = sf.get_toc()
-    print(chapter_list)
-    for chapter in chapter_list["volume_list"]:
-        print(chapter["volume_name"] + " : " + chapter["chapter_name"])
-        print(sf.get_chapters(chapter["chapter_url"]))
+app = FastAPI(
+    title="SFACG WEB API",
+)
+
+
+async def get_sfacgbook():
+    return sfacg_web_api.SFACGBook()
+
+
+@app.middleware("http")
+async def add_cors(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers[
+        "Access-Control-Allow-Headers"] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    return response
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page():
+    with open("login/login.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+
+@app.get("/api/login")
+async def login(name: str, password: str, al: str, session: str, sig: str, token: str, scene: str,
+                sfacg_book: sfacg_web_api.SFACGBook = Depends(get_sfacgbook)):
+    if not name or not password:
+        raise HTTPException(status_code=400, detail="username or password is empty")
+    if not al or not session or not sig or not token or not scene:
+        raise HTTPException(status_code=400, detail="al, session, sig, token, scene is empty")
+
+    return sfacg_book.login(name, password, al, session, sig, token, scene)
+
+
+@app.get("/api/book/{book_id}")
+async def book_info(book_id: str, sfacg_book: sfacg_web_api.SFACGBook = Depends(get_sfacgbook)):
+    if not book_id:
+        raise HTTPException(status_code=400, detail="book_id is empty")
+    book_info_json = sfacg_book.get_book_info(book_id)
+    if book_info_json:
+        return book_info_json
+    else:
+        raise HTTPException(status_code=400, detail="book_id is error")
+
+
+@app.get("/api/book/{book_id}/chapter_list")
+async def chapter_list(book_id: str, sfacg_book: sfacg_web_api.SFACGBook = Depends(get_sfacgbook)):
+    if not book_id:
+        raise HTTPException(status_code=400, detail="book_id is empty")
+    chapter_list_json = sfacg_book.get_toc(book_id)
+    if chapter_list_json:
+        return chapter_list_json
+    else:
+        raise HTTPException(status_code=400, detail="book_id is error")
+
+
+@app.get("/api/book/{book_id}/chapter/{chapter_id}")
+async def chapter(book_id: str, chapter_id: str, sfacg_book: sfacg_web_api.SFACGBook = Depends(get_sfacgbook)):
+    if not book_id or not chapter_id:
+        raise HTTPException(status_code=400, detail="book_id or chapter_id is empty")
+    chapter_json = sfacg_book.get_chapters(book_id, chapter_id)
+    if chapter_json:
+        return chapter_json
+    else:
+        raise HTTPException(status_code=400, detail="book_id or chapter_id is error")
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
--- sfacg_web_api/__init__.py
+++ sfacg_web_api/__init__.py
@@ -5,10 +5,10 @@ from retrying import retry
 
 
 class SFACGBook:
-    def __init__(self, book_id):
-        if not book_id.isdigit():
-            raise ValueError("book_id must be digit, please check your book_id")
-        self.book_id = book_id
+    LOGIN_URL = "https://passport.sfacg.com/Ajax/QuickLogin.ashx"
+    BOOK_URL = "https://book.sfacg.com/"
+
+    def __init__(self):
         self.session = requests.Session()
 
     @retry(stop_max_attempt_number=3)
@@ -21,8 +21,16 @@ class SFACGBook:
             raise ValueError(f"get {url} error, status_code: {response.status_code}")
         return etree.HTML(response.text)
 
-    def get_book_info(self):
-        html = self._get_html(f"https://book.sfacg.com/Novel/{self.book_id}/")
+    @retry(stop_max_attempt_number=3)
+    def _post_api(self, url, data):
+        response = self.session.get(url, params=data, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+        })
+        return response
+
+    def get_book_info(self, book_id):
+        html = self._get_html(f"https://book.sfacg.com/Novel/{book_id}/")
         author = html.xpath('/html/body/div[1]/div[5]/div/div[1]/div[2]/div[2]/div[2]/p[1]/text()')[0]
         description = html.xpath('/html/body/div[1]/div[4]/div/div[1]/div[2]/p/text()')[0]
         cover_url = html.xpath('//*[@id="hasTicket"]/div[1]/div/div[1]/a/img/@src')
@@ -42,9 +50,9 @@ class SFACGBook:
             'description': description.strip(),
         }
 
-    def get_toc(self):
-        response = self._get_html(f"https://book.sfacg.com/Novel/{self.book_id}/MainIndex/")
-        volume_list = {"book_id": self.book_id, "volume_list": []}
+    def get_toc(self, book_id):
+        response = self._get_html(f"https://book.sfacg.com/Novel/{book_id}/MainIndex/")
+        volume_list = {"book_id": book_id, "volume_list": []}
         volume_index = 0
         for volume in response.xpath('/html/body/div[1]/div[3]/div'):
             try:
@@ -60,20 +68,52 @@ class SFACGBook:
                      'chapter_name': chapter_name, 'chapter_url': chapter_url})
         return volume_list
 
-    def get_chapters(self, chapter_path):
+    def get_chapters(self, book_id, chapter_path):
         if "vip" in chapter_path:
             return {
-                "book_id": self.book_id,
+                "book_id": book_id,
                 "chapter_path": chapter_path,
                 "chapter_id": re.findall(r"/c/(\d+)/", chapter_path)[0],
                 "content": "VIP章节, 请使用app接口,web端无法获取正文信息"
             }
         chapter_response = self._get_html(f"https://book.sfacg.com{chapter_path}")
         return {
-            "book_id": self.book_id,
+            "book_id": book_id,
             "volume_id": chapter_path.split("/")[3],
             "chapter_id": chapter_path.split("/")[4],
             'chapter_path': chapter_path,
             'chapter_title': chapter_response.xpath('//*[@id="article"]/div[1]/h1/text()')[0],
             'content': '\n'.join(chapter_response.xpath('//*[@id="ChapterBody"]/p/text()'))
         }
+
+    def login(self, name, password, al, session, sig, token, scene):
+        if not name or not password:
+            print("name or password is empty, please check your username and password")
+            return
+        if not al or not session or not sig or not token or not scene:
+            print("al or session or sig or token or scene is empty, please check your al or session or sig or token or "
+                  "scene")
+            return
+
+        url = "https://passport.sfacg.com/Ajax/QuickLogin.ashx"
+        res = self._post_api(url, data={
+            "name": name,
+            "password": password,
+            "al": al,
+            "session": session,
+            "sig": sig,
+            "token": token,
+            "scene": scene
+        })
+        if res.status_code == 200:
+            return {
+                "code": res.status_code,
+                "message": "success",
+                "SFCommunity": res.cookies[".SFCommunity"],
+                "session_PC": res.cookies["session_PC"]
+            }
+        else:
+            return {
+                "code": res.status_code,
+                "message": "login failed, please check your username and password",
+            }
